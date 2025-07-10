b284a277daa208fea54b22e8e7feecf66a63cf39
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-07-10 14:01:24 +0900

    Fix pathes.

--- src/worker.js
+++ src/worker.js
@@ -2,18 +2,19 @@ const { parentPort, workerData } = require("worker_threads");
 const kuromoji = require("kuromoji");
 const wanakana = require("wanakana");
 
-const isAlphabetOnly = require('../src/isAlphabetOnly');
-const toHepburn = require('../src/toHepburn');
+const isAlphabetOnly = require("../lib/src/isAlphabetOnly");
+const toHepburn = require("../lib/src/toHepburn");
 
 let tokenizer;
 
 async function initializeTokenizer() {
   if (tokenizer) return tokenizer;
   const tokenizerBuilder = kuromoji.builder({ dicPath: workerData.dicPath });
-  
+
   return new Promise((resolve, reject) => {
     tokenizerBuilder.build((err, builtTokenizer) => {
-      if (err) return reject(new Error("Failed to initialize tokenizer: " + err));
+      if (err)
+        return reject(new Error("Failed to initialize tokenizer: " + err));
       tokenizer = builtTokenizer;
       resolve(tokenizer);
     });
@@ -32,15 +33,19 @@ async function initializeTokenizer() {
         if (isWordObject && word.pronounce) {
           const toHepburnString = toHepburn(word.pronounce);
           return toHepburnString;
-        };
+        }
         if (isAlphabetOnly(word)) return word;
         const targetWord = isWordObject ? word.word : word;
         const tokens = tokenizerInstance.tokenize(targetWord);
-        const hiragana = tokens.map((token) => token.reading || token.surface_form).join('');
+        const hiragana = tokens
+          .map((token) => token.reading || token.surface_form)
+          .join("");
         const toRomajiString = wanakana.toRomaji(hiragana);
         return toRomajiString;
       } catch (tokenizeErr) {
-        throw new Error(`Error processing word "${targetWord}": ${tokenizeErr}`);
+        throw new Error(
+          `Error processing word "${targetWord}": ${tokenizeErr}`
+        );
       }
     });
 
